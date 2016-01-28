f21d0fa13133fab8795297687dde76af7ca16708
Zhuyi Xue <alfred532008@gmail.com>
2016-01-28 19:04:18 +0000

    updated config.py to read input configuration from argsparser.parse_args

--- config.py
+++ config.py
@@ -1,83 +1,93 @@
 import os
+import re
 import multiprocessing
 
+from argsparser import parse_args
 
-CONFIG = {
-    # must be a google cloud storage path
-    'input_gs_bam': 'gs://tasrcloud-test-data/test.bam',
-    # 'input_gs_bam': 'gs://isb-cgc-open-zxue/CESC/DNA-Seq/C836.HEC-108.1.bam',
-    'input_gs_bf': 'gs://tasrcloud-bfs/targetUTRcell2009.tar.gz',
-    'num_cpus': multiprocessing.cpu_count(),
 
-    'steps': {
-        'biobloomcategorizer': {
-            'bf_name': 'targetUTRcell2009',
+def gen_config():
+    args = parse_args()
+    config = {
+        'input_gs_bam': args.input_gs_bam,
+        'input_gs_bf': args.input_gs_bf,
+        'num_cpus': multiprocessing.cpu_count(),
+
+        'steps': {
+            'biobloomcategorizer': {
+                'bf_name': re.sub('\.tar\.gz$', '', os.path.basename(args.input_gs_bf))
+            },
+
+            'abyss': {
+                'num_reads_cutoff': args.abyss_num_reads_cutoff,
+                'kmer_size': args.abyss_kmer_size,
+            },
+
+            'upload': {
+                'output_gs_bucket': args.upload_gs_bucket,
+            },
         },
 
-        'abyss': {
-            'kmer_size': 15,
-        },
-    },
-
-}
+    }
 
 
-if 'prefix' not in CONFIG or not CONFIG['prefix']:
-    CONFIG['prefix'] = os.path.basename(CONFIG['input_gs_bam']).rstrip('.bam')
+    if 'prefix' not in config or not config['prefix']:
+        config['prefix'] = os.path.basename(config['input_gs_bam']).rstrip('.bam')
 
-if 'output_dir' not in CONFIG or not CONFIG['output_dir']:
-    CONFIG['output_dir'] = os.path.join(os.getcwd(), 'tasrcloud_results', CONFIG['prefix'])
+    if 'output_dir' not in config or not config['output_dir']:
+        config['output_dir'] = os.path.join(os.getcwd(), 'tasrcloud_results', config['prefix'])
 
-try:
-    os.makedirs(CONFIG['output_dir'])
-except OSError:
-    pass
+    try:
+        os.makedirs(config['output_dir'])
+    except OSError:
+        pass
 
+    config['logging'] = {
+        'version': 1,
+        'disable_existing_loggers': True,
 
-CONFIG['logging'] = {
-    'version': 1,
-    'disable_existing_loggers': True,
-
-     'loggers': {
-         '__main__': {
-             'handlers': ['screen', 'file'],
-             'level': 'DEBUG',
-             'propagate': True,
-         },
-         'utils': {
-             'handlers': ['screen', 'file'],
-             'level': 'DEBUG',
-             'propagate': True,
-         },
-     },
-
-     'formatters': {
-         'verbose': {
-             'format': '%(levelname)s|%(asctime)s|%(name)s|%(module)s|%(process)d|%(processName)s|%(relativeCreated)d|%(thread)d|%(threadName)s|%(msecs)d ms|%(pathname)s+%(lineno)d|%(funcName)s:%(message)s'
-         },
-         'standard': {
-             'format': '%(levelname)s|%(asctime)s|%(name)s:%(message)s'
-         },
-         'colorful': {
-             # https://github.com/borntyping/python-colorlog#with-dictconfig
-             '()': 'colorlog.ColoredFormatter',
-             'format': '%(log_color)s%(levelname)s%(reset)s|%(log_color)s[%(asctime)s]%(reset)s|%(log_color)s%(name)s%(reset)s:%(message)s'
-         }
-     },
-
-     'handlers': {
-         'screen':{
-             'level': 'DEBUG',
-             'class': 'logging.StreamHandler',
-             'formatter': 'colorful'
-         },
-         'file': {
-             'level': 'DEBUG',
-             'class': 'logging.FileHandler',
-             'filename': os.path.join(CONFIG['output_dir'], 'tasrcloud.log'),
-             'formatter': 'standard'
+         'loggers': {
+             '__main__': {
+                 'handlers': ['screen', 'file'],
+                 'level': 'DEBUG',
+                 'propagate': True,
+             },
+             'utils': {
+                 'handlers': ['screen', 'file'],
+                 'level': 'DEBUG',
+                 'propagate': True,
+             },
          },
 
-     },
+         'formatters': {
+             'verbose': {
+                 'format': '%(levelname)s|%(asctime)s|%(name)s|%(module)s|%(process)d|%(processName)s|%(relativeCreated)d|%(thread)d|%(threadName)s|%(msecs)d ms|%(pathname)s+%(lineno)d|%(funcName)s:%(message)s'
+             },
+             'standard': {
+                 'format': '%(levelname)s|%(asctime)s|%(name)s:%(message)s'
+             },
+             'colorful': {
+                 # https://github.com/borntyping/python-colorlog#with-dictconfig
+                 '()': 'colorlog.ColoredFormatter',
+                 'format': '%(log_color)s%(levelname)s%(reset)s|%(log_color)s[%(asctime)s]%(reset)s|%(log_color)s%(name)s%(reset)s:%(message)s'
+             }
+         },
 
- }
+         'handlers': {
+             'screen':{
+                 'level': 'DEBUG',
+                 'class': 'logging.StreamHandler',
+                 'formatter': 'colorful'
+             },
+             'file': {
+                 'level': 'DEBUG',
+                 'class': 'logging.FileHandler',
+                 'filename': os.path.join(config['output_dir'], 'tasrcloud.log'),
+                 'formatter': 'standard'
+             },
+
+         },
+
+     }
+    return config
+
+CONFIG = gen_config()

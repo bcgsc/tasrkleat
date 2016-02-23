bab1326112337c27f2ce866feba195c7c6222525
Zhuyi Xue <alfred532008@gmail.com>
2016-02-23 20:07:52 +0000

    updated comment for abyss step

--- app.py
+++ app.py
@@ -100,22 +100,57 @@ def biobloomcategorizer(input_bam, outputs):
 @R.mkdir(biobloomcategorizer, R.formatter(), '{subpath[0][1]}/abyss')
 @R.transform(biobloomcategorizer, R.formatter(), [
     # '{subpath[0][1]}/abyss/coverage.hist',
+    # '{subpath[0][1]}/abyss/cba-1.dot',
     # '{subpath[0][1]}/abyss/cba-1.fa',
-    # '{subpath[0][1]}/abyss/cba-bubbles.fa',
-    # '{subpath[0][1]}/abyss/cba-1.adj',
     # '{subpath[0][1]}/abyss/cba-1.path',
+    # '{subpath[0][1]}/abyss/cba-2.dot',
+    # '{subpath[0][1]}/abyss/cba-2.dot1',
+    # '{subpath[0][1]}/abyss/cba-2.fa',
     # '{subpath[0][1]}/abyss/cba-2.path',
-    # '{subpath[0][1]}/abyss/cba-2.adj',
-    # '{subpath[0][1]}/abyss/cba-3.adj',
+    # '{subpath[0][1]}/abyss/cba-3.dist',
+    # '{subpath[0][1]}/abyss/cba-3.dot',
     # '{subpath[0][1]}/abyss/cba-3.fa',
+    # '{subpath[0][1]}/abyss/cba-3.fa.fai',
+    # '{subpath[0][1]}/abyss/cba-3.hist',
+    # '{subpath[0][1]}/abyss/cba-4.dot',
+    # '{subpath[0][1]}/abyss/cba-4.fa',
+    # '{subpath[0][1]}/abyss/cba-4.fa.fai',
+    # '{subpath[0][1]}/abyss/cba-4.path1',
+    # '{subpath[0][1]}/abyss/cba-4.path2',
+    # '{subpath[0][1]}/abyss/cba-4.path3',
+    # '{subpath[0][1]}/abyss/cba-5.dot',
+    # '{subpath[0][1]}/abyss/cba-5.fa',
+    # '{subpath[0][1]}/abyss/cba-5.path',
+    # '{subpath[0][1]}/abyss/cba-6.dist.dot',
+    # '{subpath[0][1]}/abyss/cba-6.dot',
+    # '{subpath[0][1]}/abyss/cba-6.fa',
+    # '{subpath[0][1]}/abyss/cba-6.hist',
+    # '{subpath[0][1]}/abyss/cba-6.path',
+    # '{subpath[0][1]}/abyss/cba-6.path.dot',
+    # '{subpath[0][1]}/abyss/cba-7.dot',
+    # '{subpath[0][1]}/abyss/cba-7.fa',
+    # '{subpath[0][1]}/abyss/cba-7.path',
+    # '{subpath[0][1]}/abyss/cba-8.dot',
+    # '{subpath[0][1]}/abyss/cba-8.fa',
+    # '{subpath[0][1]}/abyss/cba-bubbles.fa',
+    # # this is symlinked to cba-6.dot
+    # '{subpath[0][1]}/abyss/cba-contigs.dot',
+    # # this is symlinked to cba-6.fa
+    # '{subpath[0][1]}/abyss/cba-contigs.fa',
+    # '{subpath[0][1]}/abyss/cba-data',
+    # '{subpath[0][1]}/abyss/cba-data.tar.gz',
     # '{subpath[0][1]}/abyss/cba-indel.fa',
-    # # this is a symlink to cba-3.fa
-    # '{subpath[0][1]}/abyss/cba-unitigs.fa',
-    # '{subpath[0][1]}/abyss/cba-stats.tab',
-    # # this is a symlink to cba-stats.tab
+    # # this is symlinked to cba-8.dot
+    # '{subpath[0][1]}/abyss/cba-scaffolds.dot',
+    # # this is symlinked to cba-8.fa
+    # '{subpath[0][1]}/abyss/cba-scaffolds.fa',
+    # # this is symlinked to cba-stats.tab
     # '{subpath[0][1]}/abyss/cba-stats',
     # '{subpath[0][1]}/abyss/cba-stats.csv',
     # '{subpath[0][1]}/abyss/cba-stats.md',
+    # '{subpath[0][1]}/abyss/cba-stats.tab',
+    # # this is symlinked to cba-3.fa
+    # '{subpath[0][1]}/abyss/cba-unitigs.fa',
     '{subpath[0][1]}/abyss/abyss.log',
     '{subpath[0][1]}/abyss/abyss.COMPLETE'
 ])

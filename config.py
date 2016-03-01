05f6a06ae86165d70511cf083b301f6f8c666dd2
Zhuyi Xue <alfred532008@gmail.com>
2016-03-01 22:07:01 +0000

    adapted argsparser.py config.py utils.py

---
+++ config.py
@@ -0,0 +1,104 @@
+import os
+import re
+import multiprocessing
+
+from argsparser import parse_args
+
+
+def gen_config():
+    # project_id will be used to compose directory name in the final output
+    # result
+    project_id = 'for-ewan-polya'
+    args = parse_args()
+
+    config = {
+        'input_gs_bam': args.input_gs_bam,
+        # authorized gsutil
+        'auth_gsutil': 'gsutil -o Credentials:gs_oauth2_refresh_token=$(cat {0})'.format(args.refresh_token),
+
+        'steps': {
+            'upload': {
+                'output_gs_bucket': args.upload_gs_bucket,
+            },
+        },
+
+    }
+
+    if args.num_threads:
+        config['num_cpus'] = args.num_threads
+    else:
+        config['num_cpus'] = multiprocessing.cpu_count()
+
+    if 'prefix' not in config or not config['prefix']:
+        # trying to mimic the directory hierarchy as the input by remove gs://
+        # at the beginning and .bam at the end of the url
+        config['prefix'] = re.sub('^gs\:\/\/', '', config['input_gs_bam'])
+        config['prefix'] = re.sub('\.bam$', '', config['prefix'])
+
+    if 'output_dir' not in config or not config['output_dir']:
+        config['output_dir'] = os.path.join(
+            # use - instead of _ to avoid confusion because when downloading
+            # the zip from GCS, / will replaced with _
+            os.getcwd(), config['prefix'], '{0}-results'.format(project_id))
+
+    output_log_file = args.output_log
+    if not output_log_file:
+        output_log_file = os.path.join(
+            config['output_dir'], '{0}.log'.format(project_id))
+
+    try:
+        os.makedirs(config['output_dir'])
+    except OSError:
+        pass
+
+    config['logging'] = configure_logging_dict(output_log_file)
+    return config
+
+def configure_logging_dict(output_log_file):
+    return {
+        'version': 1,
+        'disable_existing_loggers': True,
+
+        'loggers': {
+            '__main__': {
+                'handlers': ['screen', 'file'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            'utils': {
+                'handlers': ['screen', 'file'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            },
+
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s|%(asctime)s|%(name)s|%(module)s|%(process)d|%(processName)s|%(relativeCreated)d|%(thread)d|%(threadName)s|%(msecs)d ms|%(pathname)s+%(lineno)d|%(funcName)s:%(message)s'
+                },
+            'standard': {
+                'format': '%(levelname)s|%(asctime)s|%(name)s:%(message)s'
+                },
+            'colorful': {
+                # https://github.com/borntyping/python-colorlog#with-dictconfig
+                '()': 'colorlog.ColoredFormatter',
+                'format': '%(log_color)s%(levelname)s%(reset)s|%(log_color)s[%(asctime)s]%(reset)s|%(log_color)s%(name)s%(reset)s:%(message)s'
+                }
+            },
+
+        'handlers': {
+            'screen':{
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'colorful'
+                },
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': output_log_file,
+                'formatter': 'standard'
+                },
+            },
+        }
+
+CONFIG = gen_config()
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+import datetime
+import gzip
+import subprocess
+import time
+from functools import update_wrapper
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+
+@decorator
+def timeit(f):
+    """time a function, used as decorator"""
+    def new_f(*args, **kwargs):
+        bt = time.time()
+        r = f(*args, **kwargs)
+        et = time.time()
+        logger.info("Time spent on {0}: {1:.2f}s".format(f.__name__, et - bt))
+        return r
+    return new_f
+
+
+def gzip_compress(input_file):
+    """name as such to differentiate from gzip module"""
+    with gzip.open(input_file + '.gz', 'wb') as opf:
+        with open(input_file, 'rb') as inf:
+            opf.write(inf.read())
+    os.remove(input_file)
+
+
+def touch(fname, cmd=None, times=None):
+    """
+    Similar to nix command, touch, to create an empty file, but also added some
+    meta data to the touched file
+    """
+    with open(fname, 'a') as opf:
+        opf.write('created: {0}\n'.format(datetime.datetime.now()))
+        opf.write('location of code execution: {0}\n'.format(
+            os.path.abspath('.')))
+        if cmd:
+            opf.write('{0}\n'.format(cmd))
+        os.utime(fname, times)
+
+
+def execute(cmd, flag_file=None, msg_id='#', debug=False):
+    """
+    This execute doesn't log all stdout, which could look funny, especially
+    when it comes to tools like aspc and wget
+    """
+    logger.info('{0}: executing: {1}'.format(msg_id, cmd))
+    if debug:                   # only print out cmd
+        return
+    try:
+        returncode = subprocess.call(cmd, shell=True, executable="/bin/bash")
+        msg = '{0}: returncode: {1}; CMD: "{2}"'.format(msg_id, returncode, cmd)
+        if returncode != 0:
+            logger.error(msg)
+        else:
+            logger.info(msg)
+            if flag_file is not None:
+                touch(flag_file, cmd)
+        return returncode
+    except OSError as err:
+        logger.exception(
+            '{0}: failed to start, raising OSError {1}. '
+            'CMD: "{2}"'.format(msg_id, err, cmd))

689cb483a45ef45ca7dc8c7eca3fa3003a0e9b64
Zhuyi Xue <alfred532008@gmail.com>
2016-03-16 23:17:53 +0000

    first commit

---
+++ app/app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+
+import ruffus as R
+
+from config import CONFIG
+import logging.config
+logging.config.dictConfig(CONFIG['logging'])
+logger = logging.getLogger(__name__)
+
+import utils as U
+
+import pprint
+logger.info('\n{0}'.format(pprint.pformat(CONFIG)))
+
+
+@R.transform(CONFIG['input_bam'], R.formatter(),
+    '{path[0]}/{basename[0]}.bam.bai'
+)
+@U.timeit
+def index_bam(input_bam, output_bai):
+    cmd = 'samtools index {input_bam}'.format(**locals())
+    U.execute(cmd)
+
+
+@R.follows(index_bam)
+@R.mkdir(CONFIG['input_bam'], R.formatter(), '{path[0]}/mpileup_vcf')
+@R.transform(CONFIG['input_bam'], R.formatter(),
+    '{path[0]}/mpileup_vcf/{basename[0]}.vcf'
+)
+@U.timeit
+def mpileup_vcf(input_bam, output_vcf):
+    cfg = CONFIG['steps']['mpileup_vcf']
+    cfg.update(locals())
+    cmd = ('samtools mpileup '
+           '-C50 '
+           '-ABuf '
+           '{ref_fa} '
+           '{input_bam} '
+           '| bcftools view '
+           '-cvg '
+           '- > {output_vcf}'.format(**cfg))
+    U.execute(cmd)
+
+
+@R.mkdir(mpileup_vcf, R.formatter(), '{path[0]}/find_hexamer_snvs')
+@R.transform(mpileup_vcf, R.formatter(),
+    '{path[0]}/find_hexamer_snvs/{basename[0]}.tsv',
+)
+@U.timeit
+def find_hexamer_snvs(input_vcf, output_tsv):
+    cfg = CONFIG['steps']['find_hexamer_snvs']
+    cfg.update(locals())
+    cmd = ('find_hexamer_snvs.py '
+           '{input_vcf} '
+           '{ref_fa} '
+           '{ref_gff} '
+           '{output_tsv}'.format(**cfg))
+    U.execute(cmd)
+
+
+if __name__ == "__main__":
+    R.pipeline_printout_graph(os.path.join(CONFIG['output_dir'], 'flowchart.svg'))
+    R.pipeline_run(logger=logger)
---
+++ app/argsparser.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+import ruffus as R
+
+
+def parse_args():
+    parser = R.cmdline.get_argparse(
+        description='variant-call',
+        usage='require python-2.7.x')
+
+    parser.add_argument(
+        '-i', '--input-bam', required=True,
+        help='input bam file')
+
+    parser.add_argument(
+        '--input-ref-fa', required=True,
+        help='reference genome in fasta format (e.g. hg19.fa)')
+
+    parser.add_argument(
+        '--input-ref-gff', required=True,
+        help='reference gff file')
+
+    parser.add_argument(
+        '-t', '--num-threads',
+        help=('if not specified, will use the number of available cpus on '
+              'the machine'))
+
+    parser.add_argument(
+        '--output-log',
+        help='output log file, default to <current_dir>/bamqc.log')
+    
+    args = parser.parse_args()
+    return args
